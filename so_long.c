9785fa5fb36be4095ddc42fedc8d7b1a61cbfa9a
reduno96 <reduno96@gmail.com>
2024-04-24 17:34:22 +0100

    Update so_long.c with mlx_init and mlx_loop functions

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 11:54:17 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/04/24 10:28:15 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/04/24 16:37:59 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ int	main(int argc, char **argv)
 {
 	char	**map;
 	int i;
+	void *mlx;
 
+// (void) argv;
 	i = 0;
 	map = NULL;
 	if (argc == 2)
 	{
 		map = ft_validate(argv[1]);
+		mlx = mlx_init(700, 700, "so_long", 0);
+		mlx_loop(mlx);
 
-		// while(map[i])
-		// 	printf("%s", map[i++]);
 	}
 }
